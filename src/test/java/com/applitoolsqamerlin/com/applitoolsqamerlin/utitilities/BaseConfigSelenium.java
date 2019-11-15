66285160985d44bfd986ddb78ed6f593a0e7aeb0
Julian <julian@merlinjobs.com>
2019-11-15 17:09:47 -0500

    Configure Project automation

---
+++ src/test/java/com/applitoolsqamerlin/com/applitoolsqamerlin/utitilities/BaseConfigSelenium.java
@@ -0,0 +1,50 @@
+package com.applitoolsqamerlin.com.applitoolsqamerlin.utitilities;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BaseConfigSelenium {
+
+  protected WebDriver driver;
+  protected WebDriverWait wait;
+
+  @BeforeEach
+  public void init() {
+    configCapabilities("Mac");
+  }
+
+  @AfterEach
+  public void teardown() {
+    driver.close();
+    driver.quit();
+  }
+
+  public void navigateTo(String url) {
+    driver.get(url);
+  }
+
+  public void configCapabilities(String flagOS) {
+    ChromeOptions options = new ChromeOptions();
+//        options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
+//    	options.addArguments("--headless");
+
+    if(flagOS == "Windows") {
+      System.setProperty("webdriver.chrome.driver", "chromedriverWIN.exe");
+      driver = new ChromeDriver();
+    } else if(flagOS == "Mac") {
+      System.setProperty("webdriver.chrome.driver", "chromedriver");
+      driver = new ChromeDriver();
+    }
+
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    driver.manage().window().fullscreen();
+    wait = new WebDriverWait(driver, 60);
+  }
+
+
+}
